010ec3c1943d8ab11d7e9be7fe181b6f4c1073cf
Sean Evans <wseanevans@gmail.com>
2018-02-15 10:32:01 -0600

    Correction

---
+++ imperial_to_metric_height.js
@@ -0,0 +1,59 @@
+// var myName = prompt('What is your name?');
+
+// var heightInches = prompt("What is your height in inches?");
+
+// var weightPounds = prompt("What is your weight in pounds?");
+
+// var heightCentimeters = heightInches * 2.54;
+
+// var weightKilograms = weightPounds * 0.453592;
+
+// console.log(myName + ' is ' + heightCentimeters + ' cm and ' + weightKilograms + ' kg.');
+
+
+// var myName = prompt("What is your name?");
+// var newLang = prompt("Do you know a language besides English?");
+// var welcomeSaying = prompt("How do you greet others in that language?")
+
+// function greeting(name, language, saying) {
+// 	console.log(saying + " " + name + ", nice to speak with someone who knows " + language);
+// }
+
+// greeting(myName, newLang, welcomeSaying);
+
+
+
+
+
+var name = prompt("Pick a name!");
+
+var fav_sport = prompt("Pick a sport!");
+
+var state = prompt("Pick a state!");
+
+var celebrity = prompt("Pick a celebrity!");
+
+var fav_fruit = prompt("Pick a fruit");
+
+var body = prompt("Pick a body part!");
+
+var clothing_brand = prompt("Pick a clothing brand!");
+
+var book = prompt("Pick a book!");
+
+var color = prompt("Pick an adjective");
+
+var crypto = prompt("Pick a cryptocurrency!");
+
+function mad_lib(a,b,c,d,e,f,g,h,i,j) {     
+	console.log(a + " likes to play " + b + " in " + c + " While hanging out with " + d + " eating "+ e + " on " + d + "'s " + f + " wearing " + g + " while reading " + h + " which is a " + i + " book about " + j) 
+	// console.log(a + " likes to play " + b + " in " + c + " while hanging out with " + d + " eating " + e + " on " + )
+	};
+
+mad_lib(name,fav_sport,state,celebrity,fav_fruit,body,clothing_brand,book,color,crypto);
+
+
+
+
+
+
